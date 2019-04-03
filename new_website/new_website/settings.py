71c67be255f0e7be60751ac632a98b2219a4cf91
ChristianGroeber <christian.groeber@bluewin.ch>
2019-04-03 08:20:25 +0200

    added static files

--- new_website/new_website/settings.py
+++ new_website/new_website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'website'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_DIRS = (
+    os.path.join(SETTINGS_PATH, 'templates'),
+)
+
+STATICFILES_DIRS = [
+    os.path.join(SETTINGS_PATH, "static"),
+]
