ad45a6d974ccd5416ab5c7c7713c299b67dc66c7
ChristianGroeber <christian.groeber@bluewin.ch>
2019-04-03 12:35:35 +0200

    added content and rows to models

--- new_website/website/models.py
+++ new_website/website/models.py
@@ -3,12 +3,24 @@ from django.db import models
 # Create your models here.
 
 
+class Row(models.Model):
+    title = models.CharField(max_length=20, blank=True)
+    html_id = models.CharField(max_length=10, blank=True)
+    text = models.CharField(max_length=1000, blank=True)
+    # image = models.ImageField()
+
+
+class Content(models.Model):
+    rows = models.ManyToManyField(Row)
+
+
 class Page(models.Model):
     name = models.CharField(max_length=20)
     url = models.CharField(max_length=20)
     title = models.CharField(max_length=200, blank=True)
     description = models.CharField(max_length=200, blank=True)
     show_on_page = models.BooleanField(default=True)
+    content = models.ManyToManyField(Content, blank=True)
 
     def __str__(self):
         return self.name
