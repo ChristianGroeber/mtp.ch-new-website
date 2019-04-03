6c06f83560f684f11dfaba66e75ef36fd0c76158
ChristianGroeber <christian.groeber@bluewin.ch>
2019-04-03 12:15:49 +0200

    small changes on models, added Pages to models.py

--- new_website/website/models.py
+++ new_website/website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Page(models.Model):
+    name = models.CharField(max_length=20)
+    url = models.CharField(max_length=20)
+    title = models.CharField(max_length=200, blank=True)
+    description = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
