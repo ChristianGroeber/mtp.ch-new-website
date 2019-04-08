7c2a5c3402d804b39c2e008e190d0aa5cc5d157a
ChristianGroeber <christian.groeber@bluewin.ch>
2019-04-08 11:04:32 +0200

    first overview setup

--- new_website/Coaches/admin.py
+++ new_website/Coaches/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SportType, TrainingMethod, Coach
+
+admin.site.register(SportType)
+admin.site.register(TrainingMethod)
+admin.site.register(Coach)
--- new_website/Coaches/views.py
+++ new_website/Coaches/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Coach
 
 
 def overview(request):
-    return render(request, 'coaches/overview.html')
+    coaches = Coach.objects.all()
+    return render(request, 'coaches/overview.html', {'coaches': coaches})
